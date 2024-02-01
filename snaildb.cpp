219d9bc3a747743dde04487b70719cc7b1a58eda
Henry Hamon <henry@hamonrails.com.br>
2024-02-01 12:49:32 -0300

    ✨ Add cursor and multidimensional data

--- snaildb.cpp
+++ snaildb.cpp
@@ -3,9 +3,25 @@
 
 SnailDef::~SnailDef()
 {
-    for (auto &dataType : dataTypes)
+    // Delete allocated memory for data
+    for (size_t i = 0; i < numRows; ++i)
     {
-        delete dataType;
+        for (size_t j = 0; j < numCols; ++j)
+        {
+            delete data[i][j];
+        }
+        delete[] data[i];
+    }
+    delete[] data;
+}
+
+void SnailDef::initializeData()
+{
+    // Initialize the two-dimensional array
+    data = new BaseSDataType **[numRows];
+    for (size_t i = 0; i < numRows; ++i)
+    {
+        data[i] = new BaseSDataType *[numCols];
     }
 }
 
@@ -13,7 +29,7 @@ void SnailDef::addStrColProp(const std::string &colName, size_t max_length)
 {
     try
     {
-        dataTypes.push_back(new StrCol(max_length));
+        data[cursor][colNames.size()] = new StrCol(max_length);
         colNames.push_back(colName);
     }
     catch (const std::exception &e)
@@ -26,7 +42,7 @@ void SnailDef::addIntColProp(const std::string &colName, size_t max_length)
 {
     try
     {
-        dataTypes.push_back(new IntCol(max_length));
+        data[cursor][colNames.size()] = new IntCol(max_length);
         colNames.push_back(colName);
     }
     catch (const std::exception &e)
@@ -38,10 +54,10 @@ void SnailDef::addIntColProp(const std::string &colName, size_t max_length)
 std::string SnailDef::getRow() const
 {
     std::string result;
-    for (size_t i = 0; i < dataTypes.size(); ++i)
+    for (size_t i = 0; i < numCols; ++i)
     {
-        result += dataTypes[i]->getValue();
-        if (i != dataTypes.size() - 1)
+        result += data[cursor][i]->getValue();
+        if (i != numCols - 1)
         {
             result += separator;
         }
@@ -49,11 +65,42 @@ std::string SnailDef::getRow() const
     return result;
 }
 
-BaseSDataType *SnailDef::getProp(size_t index) const
+BaseSDataType *SnailDef::getProp(size_t col) const
 {
-    if (index < dataTypes.size())
+    if (cursor < numRows && col < numCols)
     {
-        return dataTypes[index];
+        return data[cursor][col];
     }
     return nullptr;
+}
+
+void SnailDef::next()
+{
+    if (cursor < numRows - 1)
+    {
+        ++cursor;
+    }
+}
+
+void SnailDef::previous()
+{
+    if (cursor > 0)
+    {
+        --cursor;
+    }
+}
+
+void SnailDef::tail()
+{
+    cursor = numRows - 1;
+}
+
+size_t SnailDef::getCursor() const
+{
+    return cursor;
+}
+
+size_t SnailDef::getSize() const
+{
+    return numRows;
 }
--- snaildb.h
+++ snaildb.h
@@ -12,17 +12,27 @@ class SnailDef
 {
 public:
     virtual ~SnailDef();
-    char separator = '|';
-
     virtual void addStrColProp(const std::string &colName, size_t max_length);
     virtual void addIntColProp(const std::string &colName, size_t max_length);
 
-    virtual std::string getRow() const;
-    BaseSDataType *getProp(size_t index) const;
+    std::string getRow() const;
+    BaseSDataType *getProp(size_t col) const;
+    void next();
+    void previous();
+    void tail();
+    size_t getCursor() const;
+    size_t getSize() const;
 
 protected:
-    std::vector<BaseSDataType *> dataTypes;
-    std::vector<std::string> colNames;
+  BaseSDataType ***data;
+  std::vector<std::string> colNames;
+  char separator = '|';
+  size_t numRows; // Number of rows
+  size_t numCols; // Number of columns
+  size_t cursor;  // Current cursor position
+
+  // Helper function to initialize the two-dimensional array
+  void initializeData();
 };
 
 #endif // SNAILDB_H
